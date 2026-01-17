9f59b3a6278b73c35396b188666eb907a1c2285f
PatelNik8 <niketpatel200424@gmail.com>
2026-01-17 09:02:21 +0530

    Unsaved changes

--- C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fb07c276e689176a151a56c3cbf74a17265e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77f83b454ba8f8a06c847abc14bca1e6682e5bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
